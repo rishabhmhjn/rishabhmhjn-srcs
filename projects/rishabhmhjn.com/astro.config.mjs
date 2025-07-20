1cf871d9c038f78cc03e996624fd7c0cd54a78a9
Rishabh Mahajan <rishabh@statusbrew.com>
2025-07-20 19:28:51 +0200

    feat: add Astro framework and initial project setup within nx
    https://docs.astro.build/en/install-and-setup/#manual-setup

---
+++ projects/rishabhmhjn.com/astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  srcDir: 'app/src',
+  publicDir: 'app/public',
+  outDir: '../../dist/apps/rishabhmhjn.com',
+});
